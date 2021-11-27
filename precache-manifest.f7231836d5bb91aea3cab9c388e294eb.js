62e8e0b927f524b27bfb78fb4522eebbd50c2656
Suyash Sawant <suyashsawant813@gmail.com>
2021-11-27 18:11:26 +0530

    Updates

---
+++ precache-manifest.f7231836d5bb91aea3cab9c388e294eb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522b34640a9b10833070583f67ccfca8",
+    "url": "/keeper/index.html"
+  },
+  {
+    "revision": "30f7bddffbbc0c0113dc",
+    "url": "/keeper/static/js/2.892ae629.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper/static/js/2.892ae629.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aca77e3468da1e609141",
+    "url": "/keeper/static/js/main.bbb5241d.chunk.js"
+  },
+  {
+    "revision": "19c518929ff0e4c8dc4e",
+    "url": "/keeper/static/js/runtime-main.dcceea5e.js"
+  }
+]);
